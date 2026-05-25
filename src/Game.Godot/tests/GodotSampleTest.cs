93f8406625c0ccd24a75f0dd503f20998a95c3e8
rykehg2 <rykehg@gmail.com>
2026-05-25 18:32:40 -0300

    ajustes

--- src/Game.Godot/tests/GodotSampleTest.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using GdUnit4;
-
-[TestSuite]
-public partial class GodotSampleTest
-{
-    [TestCase]
-    public void Should_Pass_Godot_Interaction() => Assertions.AssertBool(true).IsTrue();
-}
